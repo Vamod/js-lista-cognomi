30598c9a80def65e3b92035387ea27c720d113df
Valerio <valerio.modesti@gmail.com>
2020-07-13 16:33:43 +0200

    js-lista-cognomi

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ var cognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 
 // chiedere il cognome
 var cognomeUtente = prompt('inserisci il tuo cognome');
+
+//capitalize
+var primoCarattere = cognomeUtente.charAt(0).toUpperCase();
+var restoParola = cognomeUtente.slice(1).toLowerCase();
+var capitalize = primoCarattere + restoParola;
+
+//inserire il cognome nel array
+cognomi.push(capitalize);
+
+// ordinare la lista in ordine alfabetico
+cognomi.sort();
+
+//stampare la lista
+for(i = 0; i < cognomi.length; i++){
+    document.getElementById('lista-cognomi').innerHTML += '<li>' + cognomi[i] + ' è di indice ' + (i + 1) + '</li>';
+}
